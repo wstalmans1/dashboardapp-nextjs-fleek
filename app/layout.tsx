4997314b8cddd2f1fb2f66d0ee0beb54b9f72238
Wim Stalmans <wim.stalmans@gmail.com>
2024-06-06 13:15:16 +0200

    moved global.css to UI folder

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,4 +1,4 @@
-import './globals.css'
+import './ui/globals.css'
 import { Inter } from 'next/font/google'
 
 const inter = Inter({ subsets: ['latin'] })
--- app/page.tsx
+++ app/page.tsx
@@ -17,7 +17,7 @@ export default function Page() {
 
 
           <p className={`text-xl text-gray-800 md:text-3xl md:leading-normal`}>
-            <strong>Welcome to Acme 3.</strong> This is the example for the{' '}
+            <strong>Welcome to Acme 4.</strong> This is the example for the{' '}
             <a href="https://nextjs.org/learn/" className="text-blue-500">
               Next.js Learn Course
             </a>
