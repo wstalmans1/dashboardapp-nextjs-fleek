e1e5d7981f22c90a42c66c0ad2e82b2c4a6d837c
Wim Stalmans <wim.stalmans@gmail.com>
2024-06-07 07:29:58 +0200

    common view for dahsboard

--- app/page.tsx
+++ app/page.tsx
@@ -19,7 +19,7 @@ export default function Page() {
 
 
           <p className={`text-xl text-gray-800 md:text-3xl md:leading-normal`}>
-            <strong>Welcome to Acme 9.</strong> This is the example for the{' '}
+            <strong>Welcome to Acme 10.</strong> This is the example for the{' '}
             <a href="https://nextjs.org/learn/" className="text-blue-500">
               Next.js Learn Course
             </a>
