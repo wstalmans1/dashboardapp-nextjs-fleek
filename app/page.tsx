68cc188342447748081eadafe343ee5d4e8d3e8a
Wim Stalmans <wim.stalmans@gmail.com>
2024-06-08 22:02:28 +0200

    invoices page

--- app/page.tsx
+++ app/page.tsx
@@ -7,7 +7,7 @@ export default function Page() {
     <main className="flex min-h-screen flex-col p-6">
       
       <div className="flex h-20 shrink-0 items-end rounded-lg bg-blue-500 p-4 md:h-52">
-        <Image src="/favicon.png" alt="Example Image" className="rounded-lg" />
+        <Image src="/favicon.png" alt="Example Image" className="rounded-lg" width={32} height={32} />
           {/* <AcmeLogo /> */}
       </div>
 
